4f98862287f8ecf9193c8ee92853c56c00d024ac
Ulises <ulicossetto@hotmail.com>
2023-02-01 12:38:09 -0300

    Cambios: -Termine user, solo falta hacerle la conexion con el modelo ShoppingCart - Termine los controllers de User

--- PF_HENRY_VET/api/src/routes/UserRoutes/deleteUser.js
+++ PF_HENRY_VET/api/src/routes/UserRoutes/deleteUser.js
@@ -1,30 +1,40 @@
 const {Router} = require("express");
 const {User} = require("../../db");
+const {findUserToDelete} = require("../../controllers/controllerUsers/controllerDelete");
 const router = Router();
 
-router.post("/unsubscribe/:idUser", async (req, res) => {
+router.put("/unsubscribe/:idUser", async (req, res) => {
     const {idUser} = req.params;
     try{
+        const info = await findUserToDelete(idUser);
         if(!idUser) res.status(200).json({
             ok: false, 
             msg: "Hacen Falta Datos.",
             detail: "Falta El Id De Usuario."
         }) 
-        const unsubscribedUser = await User.update({unsubscribe_U: true}, {
-            where: {
-                cod_User: idUser
-            }
+        if(!info) res.status(200).json({
+            ok: false, 
+            msg: "No Se Encontro El Usuario.",
+            detail: "El Usuario No Existe O Se Elimino Previamente."
         })  
-        unsubscribedUser[0] ? res.status(200).json({
-            ok: true,
-            value: "Se Ha Borrado El Usuario."
-        }) 
-        :
-        res.status(200).json({
-            ok: false,
-            msg: "No Se Ha Podido Dar De Baja El Usuario.",
-            detail: "No Se Ha Dado De Baja."
-        })
+        else {
+            const unsubscribedUser = await User.update({unsubscribe_U: true}, {
+                where: {
+                    cod_User: idUser
+                }
+            })  
+            unsubscribedUser[0] ? res.status(200).json({
+                ok: true,
+                value: "Se Ha Borrado El Usuario."
+            }) 
+            :
+            res.status(200).json({
+                ok: false,
+                msg: "No Se Ha Podido Dar De Baja El Usuario.",
+                detail: "No Se Ha Dado De Baja."
+            })
+        }
+
     }catch(err){
         res.status(404).send({
             ok: false, 
