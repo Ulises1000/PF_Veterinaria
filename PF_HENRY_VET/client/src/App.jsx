d9475365e520efa2d5db1608eef8247c8830586a
santiellis <Santiagoellis@hotmail.com.ar>
2023-02-02 03:32:11 -0300

    Searchbar and  filter body

--- PF_HENRY_VET/client/src/App.jsx
+++ PF_HENRY_VET/client/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Nav from "./components/Nav.jsx"
+import Searchbar from './components/Searchbar.jsx'
+import Caracteristic from './components/Caracteristic'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
          <Nav/>
+         <Searchbar/>
+         <Caracteristic/>
     </div>
   )
 }
---
+++ PF_HENRY_VET/client/src/components/Searchbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function Searchbar() {
+  const [nameSearch, setNameSearch] = useState("");
+
+  function handleChange(e) {
+    setNameSearch(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(nameSearch);
+    setNameSearch("");
+    e.target.placeholder = "Search...";
+  }
+  return (
+    <div className="bg-violet-100 ">
+      <form
+        id="Form"
+        className="focus:border focus:bg-red-400 focus:border-red-500"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <div className="flex mb-6 px-2">
+          <input
+            type="text"
+            onChange={handleChange}
+            value={nameSearch}
+            className="bg-violet-100 px-6 text-gray-600 text-sm w-full focus:outline-none focus:text-gray-700 placeholder:focus:text-gray-700 placeholder:focus:font-medium focus:font-medium"
+            placeholder="Search..."
+          />
+          <button
+            type="submit"
+            className="flex items-center py-4 px-3  text-gray-600 hover:text-black"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-5 h-5 items-center text-gray-600 hover:text-black"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Searchbar;
