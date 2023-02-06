9117b5fe8ef3e645bfa924f74710bcd7df75cd5a
JoelGalvez20 <101646544+JoelGalvez20@users.noreply.github.com>
2023-02-06 15:37:18 -0300

    Update Breed.js
    comentar propiedades

--- PF_HENRY_VET/api/src/models/Breed.js
+++ PF_HENRY_VET/api/src/models/Breed.js
@@ -3,16 +3,16 @@ const {DataTypes} = require("sequelize");
 module.exports = (sequelize) => {
     sequelize.define("breed", {
         /*numero codificado con UUIDV4 para identificar la raza*/
-        id_breed:{
-            type:DataTypes.UUID,
-            allowNull:false,
-            defaultValue:DataTypes.UUIDV4
-        },
+//         id_breed:{
+//             type:DataTypes.UUID,
+//             allowNull:false,
+//             defaultValue:DataTypes.UUIDV4
+//         },
         /*Especie*/
-        kind:{
-            type:DataTypes.STRING,
-            allowNull:false
-        },
+//         kind:{
+//             type:DataTypes.STRING,
+//             allowNull:false
+//         },
         /*Nombre*/
         name:{
             type:DataTypes.STRING,
@@ -20,15 +20,15 @@ module.exports = (sequelize) => {
             allowNull:false
         },
         /*Rango de Peso*/
-        WeightRange:{
-            type:DataTypes.STRING,
-            allowNull:false
-        },
-        /*Rango de Altura*/
-        HeightRange:{
-            type:DataTypes.STRING,
-            allowNull:false
-        },
+//         WeightRange:{
+//             type:DataTypes.STRING,
+//             allowNull:false
+//         },
+//         /*Rango de Altura*/
+//         HeightRange:{
+//             type:DataTypes.STRING,
+//             allowNull:false
+//         },
         /*Dar de baja*/
         unsuscribe:{
             type:DataTypes.BOOLEAN,
