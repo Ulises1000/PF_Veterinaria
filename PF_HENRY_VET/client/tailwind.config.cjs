8029dedc49ba0ca06f98010ccea19b2f78e03b88
santiellis <Santiagoellis@hotmail.com.ar>
2023-02-06 00:53:49 -0300

    Landing Css Modifications

--- PF_HENRY_VET/client/tailwind.config.cjs
+++ PF_HENRY_VET/client/tailwind.config.cjs
@@ -3,8 +3,23 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      spacing: {
+        '28.125': '28.125rem',
+        "400px": "25rem",
+        "450px": "28.125rem",
+        "200px": "12.5rem",
+        "250px": "15.625rem",
+        "415px":"25.938rem"
+      },
+      colors: {
+        'Dark-Violet': 'rgba(165, 94, 234, 0.35)',
+        "Dark-Violet2": "rgb(193, 168, 228)",
+      },
       backgroundImage: {
         patas: "url('./src/style-assets/bg-patas.png')",
+      },
+      fontFamily: {
+        'Fredoka': ['"Fredoka"', '"sans-serif"'],
       },
     },
   },
