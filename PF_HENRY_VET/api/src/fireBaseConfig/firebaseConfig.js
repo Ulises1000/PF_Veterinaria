e8876607aa0a45cce5e3cf0357eb5faa5e06fdd3
Harvey Zambrano <78236849+harveyzambrano@users.noreply.github.com>
2023-02-11 13:31:52 -0500

    Update firebaseConfig.js

--- PF_HENRY_VET/api/src/fireBaseConfig/firebaseConfig.js
+++ PF_HENRY_VET/api/src/fireBaseConfig/firebaseConfig.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+require('dotenv').config();
 const { initializeApp } = require("firebase/app");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
