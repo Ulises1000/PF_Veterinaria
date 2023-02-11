0b42c3d97ebad6f12033d95e2980e40338060373
Ulises <ulicossetto@hotmail.com>
2023-02-11 06:24:21 -0300

    Cambios: -modifique el reducer para que sea mas legible

--- PF_HENRY_VET/client/src/redux/reducer/index.jsx
+++ PF_HENRY_VET/client/src/redux/reducer/index.jsx
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
-import { productsReducer, userReducer, filters } from './reducer.jsx';
+import { productsReducer, filters } from "./productAndFilterReducer.jsx";
+import { userReducer } from "./userReducer.jsx";
+import { favoriteReducer } from "./favoriteReducer.jsx";
 
 const rootReducer = combineReducers({
     products: productsReducer,
     user: userReducer,
     filters: filters,
+    favorites: favoriteReducer
 });
 
 export default rootReducer;
--- PF_HENRY_VET/client/src/redux/reducer/productAndFilterReducer.jsx
+++ PF_HENRY_VET/client/src/redux/reducer/productAndFilterReducer.jsx
@@ -13,33 +13,13 @@ import {
   BY_ORDER_PRICE,
   BY_ORDER_STOCK,
 } from "../action/constants";
-import {
-  GET_USER,
-  DELETE_USER,
-  REGISTER_USER,
-  SIGNIN_USER,
-  SIGNOUT_USER,
-  REGISTER_ERRORS,
-  SIGNIN_ERRORS,
-  CLEAN_MSG_REGISTER_USER,
-  POST_USER,
-  UPDATE_USER,
-} from "../action/constants";
-import {
-  GET_FAVORITES,
-  UPDATE_FAVORITE,
-  POST_FAVORITES
-} from "../action/constants";
+
 import { ASCENDENTE, DESCENDENTE } from "../../const/orderByName";
 
 const initialState = {
   products: [],
   product: {},
-  filterProducts:[], 
-  user: {},
-  infoRegistration: {},
-  userMsgErrorRegistrationAndSignin: "",
-  favorites: [],
+  filterProducts:[],
   currentOrder: "Static",
   currentBreed: "breed",
   currentSearch: "",
@@ -95,89 +75,6 @@ export const productsReducer = (state = initialState.products, action) => {
             ? { ...product, ...action.payload }
             : product
         ),
-      };
-    default:
-      return state;
-  }
-};
-
-export const userReducer = (state = initialState.user, action) => {
-  switch (action.type) {
-    case GET_USER:
-      return {
-        ...state,
-        user: action.payload,
-      };
-    case REGISTER_USER:
-      return {
-        ...state,
-        infoRegistration: action.payload,
-      };
-    case SIGNIN_USER:
-      return {
-        ...state,
-        user: action.payload,
-      };
-    case REGISTER_ERRORS:{
-      let msg;
-      
-      switch(action.payload){
-        case "auth/email-already-in-use": msg = "Este Email Ya Esta En Uso.";
-          break;
-        case "auth/invalid-email": msg = "Este Email Es Invalido.";
-          break;
-        case "auth/weak-password": msg = "Esta Contraseña Es Muy Débil.";
-          break;
-        default: msg = "Hubo Un Error.";
-      }
-
-      return {
-        ...state,
-        userMsgErrorRegistrationAndSignin: msg,
-      };
-    }
-    case SIGNIN_ERRORS:{
-      let msg;
-      
-      switch(action.payload){
-        case "auth/email-already-in-use": msg = "Este Email Ya Esta En Uso.";
-          break;
-        case "auth/invalid-email": msg = "Este Email Es Invalido.";
-          break;
-        case "auth/weak-password": msg = "Esta Contraseña Es Muy Débil.";
-          break;
-        default: msg = "Hubo Un Error.";
-      }
-
-      return {
-        ...state,
-        userMsgErrorRegistrationAndSignin: msg,
-      };
-    }
-    //PORAHORA EL SIGNOUT SOLO DEVUELVE EL STATE PERO SI DESLOGUEA AL USUARIO EN FIREBASE
-    case SIGNOUT_USER:
-      return {
-        ...state
-      };
-    case CLEAN_MSG_REGISTER_USER: 
-      return {
-        ...state,
-        userMsgErrorRegistrationAndSignin: "",
-      }
-    case DELETE_USER:
-      return {
-        ...state,
-        user: {},
-      };
-    case POST_USER:
-      return {
-        ...state,
-        user: action.payload,
-      };
-    case UPDATE_USER:
-      return {
-        ...state,
-        user: { ...state.user, ...action.payload },
       };
     default:
       return state;
@@ -388,26 +285,3 @@ export const filters = (state = initialState, action) => {
       return state;
   }
 };
-
-export const favoriteReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case GET_FAVORITES:
-      return {
-        ...state,
-        favorites: action.payload,
-      };
-    case UPDATE_FAVORITE:
-      return {
-        ...state,
-        favorites: action.payload,
-      };
-      case POST_FAVORITES: 
-        return {
-          ...state,
-          favorites: action.payload,
-        };
-    default:
-      return state;
-  }
-};
-
