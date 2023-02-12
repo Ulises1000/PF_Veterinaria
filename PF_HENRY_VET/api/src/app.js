a78355a11eb9d3630f4205d09942ebd0acf86565
Ulises <ulicossetto@hotmail.com>
2023-02-12 09:57:41 -0300

    Cambios: solo comente el cors para probar

--- PF_HENRY_VET/api/src/app.js
+++ PF_HENRY_VET/api/src/app.js
@@ -23,6 +23,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+/* 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');//ponele 'http://localhost:3000' // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
@@ -30,7 +31,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next(); 
 });
-
+*/
 server.use('/', routes);
 
 // Error catching endware.
